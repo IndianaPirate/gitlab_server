356eb3e06b2a6424242f3a27b546dc100e76b4a2
NIAINA Andriharisoa <andriharisoaniaina@gmail.com>
2023-08-17 22:24:19 +0200

    change port

--- deploy_gitlab.sh
+++ deploy_gitlab.sh
@@ -10,7 +10,7 @@ export GITLAB_HOME=/srv/gitlab
 
 docker run --detach \
   --hostname gitlab.example.com \
-  --publish 443:443 --publish 80:80 --publish 22:22 \
+  --publish 4443:443 --publish 80:80 --publish 22:22 \
   --name gitlab \
   --restart always \
   --volume $GITLAB_HOME/config:/etc/gitlab \
